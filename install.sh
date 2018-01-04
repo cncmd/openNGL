e9a2711efe6fcdaabc97b0446b0d091ed7902c00
wangwenbo <wangwenbo@alibaba-inc.com>
2018-01-04 17:06:27 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 OPENNGL_INSTALL_PATH=`pwd`
 OPENNGL_HOME=/home/admin/
+OPENNGL_NGX_GR=$OPENNGL_HOME/nginx
+
 #==========openresty==========
 USER=`id | awk '{print(substr($2,5,1))}'`
 
@@ -15,14 +17,23 @@ if [ $OPEN_RESTY == "" ] ; then
 	echo "openresty install"
 fi
 
+
+#============Groovy ================
 if [ ! -d  $OPENNGL_HOME ] ; then 
-	mkdir -p $OPENNGL_HOME/jars
-	mkdir -p $OPENNGL_HOME/groovy
-	mkdir -p $OPENNGL_HOME/lua
-	mkdir -p $OPENNGL_HOME/oper
-	
 	echo "work space install"
 fi
+mkdir -p $OPENNGL_HOME/jars
+mkdir -p $OPENNGL_HOME/groovy
+mkdir -p $OPENNGL_HOME/lua
+mkdir -p $OPENNGL_HOME/oper
+
+if [ ! -d $OPENNGL_NGX_GR ] ; then
+	TARGET=$OPENNGL_INSTALL_PATH/nginx/nginx.tar.gz
+	cp -f $TARGET $OPENNGL_HOME
+	cd $OPENNGL_HOME &&  tar zxf nginx.tar.gz && mv * nginx
+fi
+
+
 
 
 
