2a3a1c492a29e579cec9da2089e94626a915abc0
wangwenbo <wangwenbo@alibaba-inc.com>
2018-01-04 16:36:51 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
-echo hellow
+#!/bin/bash
+OPENNGL_INSTALL_PATH=`pwd`
+echo $OPENNGL_INSTALL_PATH
+#==========openresty==========
+USER=`id | awk '{print(substr($2,5,1))}'`
+
+if [ "$USER" == "0" ] ; then
+	 echo  "please use root"
+	 exit 0
+fi
+
+rpm -i $OPENNGL_INSTALL_PATH/nginx/*.rpm
+
+
