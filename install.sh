de08e36bf00b209207268da07a37d762fc4062f2
wangwenbo <wangwenbo@alibaba-inc.com>
2018-01-04 16:53:54 +0800

    mkdir

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 OPENNGL_INSTALL_PATH=`pwd`
-echo $OPENNGL_INSTALL_PATH
+OPENNGL_HOME=/home/admin/
 #==========openresty==========
 USER=`id | awk '{print(substr($2,5,1))}'`
 
 if [ "$USER" == "0" ] ; then
 	 echo  "please use root"
-	 exit 0
 fi
 
-rpm -i $OPENNGL_INSTALL_PATH/nginx/*.rpm
+OPEN_RESTY=`rpm -qa | grep openresty`
+
+if [ $OPEN_RESTY == "" ] ; then
+	rpm -i $OPENNGL_INSTALL_PATH/nginx/*.rpm
+	echo "openresty install"
+fi
+
+if [ ! -d  $OPENNGL_HOME ] ; then 
+	mkdir -p $OPENNGL_HOME/jars
+	mkdir -p $OPENNGL_HOME/groovy
+	mkdir -p $OPENNGL_HOME/lua
+	mkdir -p $OPENNGL_HOME/oper
+	
+	echo "work space install"
+fi
+
 
 
